f909e1fdc7c5f2b777eeaebd724fa20f877d82e3
krnirmal2 <nirmalkrmajhi14@gmail.com>
2025-03-29 11:38:58 +0530

    Bfs traversal grahp

--- src/main/java/StandardProblemDSA/X_GRAPH/BASIC_TRAVERSAL/BfsTraversal.java
+++ src/main/java/StandardProblemDSA/X_GRAPH/BASIC_TRAVERSAL/BfsTraversal.java
@@ -10,7 +10,7 @@ class Graphs {
   Graphs(int v) {
     V = v;
     adj = new LinkedList[v];
-    for (int i = 0; i < v; i++) adj[i] = new LinkedList<Integer>();
+    for (int i = 0; i < v; i++) adj[i] = new LinkedList<>();
   }
 
   void addEdge(int v, int w) {
